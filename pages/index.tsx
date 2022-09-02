e0762c577988472475b399b9e6a6c7ea48e6e67f
agruezo <agruezo@gmail.com>
2022-09-02 11:38:28 -0400

    fixed import error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,38 @@
-import Head from 'next/head'
-import { PostCard, Categories, PostWidget } from '../components/Index';
-import { getPosts }  from '../services/Index';
-import { FeaturedPosts } from '../sections/index';
+import Head from "next/head";
+import { PostCard, Categories, PostWidget } from "../components/Index";
+import { getPosts } from "../services/Index";
+import { FeaturedPosts } from "../sections/Index";
 
 const Home = ({ posts }: any) => {
-  return (
-    <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Dev IT Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <FeaturedPosts />
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1">
-          {posts.map((post: any, index: any) =>  (
-            <PostCard key={index} post={post.node} /> 
-          ))}
-        </div>
-        <div className="lg:col-span-4 col-span-1">
-            <div className="lg:sticky relative top-8">
-              <PostWidget />
-              <Categories />
+    return (
+        <div className="container mx-auto px-10 mb-8">
+            <Head>
+                <title>Dev IT Blog</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <FeaturedPosts />
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+                <div className="lg:col-span-8 col-span-1">
+                    {posts.map((post: any, index: any) => (
+                        <PostCard key={index} post={post.node} />
+                    ))}
+                </div>
+                <div className="lg:col-span-4 col-span-1">
+                    <div className="lg:sticky relative top-8">
+                        <PostWidget />
+                        <Categories />
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
-    </div>
-  );
-}
+    );
+};
 
 export async function getStaticProps() {
-  const posts = (await getPosts()) || [];
-  return {
-    props: { posts }
-  }
+    const posts = (await getPosts()) || [];
+    return {
+        props: { posts },
+    };
 }
 
-export default Home
+export default Home;
